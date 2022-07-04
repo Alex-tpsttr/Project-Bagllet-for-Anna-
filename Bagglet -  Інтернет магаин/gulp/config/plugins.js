3f0329b758a65565ad4179244688d45e5b1b62c1
Frichko Oleksandr <frichko18@icloud.com>
2022-07-05 00:58:50 +0300

    Проста перевірка

---
+++ Bagglet -  Інтернет магаин/gulp/config/plugins.js
@@ -0,0 +1,16 @@
+import replace from "gulp-replace";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import browsersync from "browser-sync";
+import newer from "gulp-newer";
+import ifPlugin from "gulp-if";
+
+// Експортуєм обєкт
+export const plugins = {
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
+    browsersync: browsersync,
+    newer: newer,
+    if: ifPlugin,
+}
